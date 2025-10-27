1fae7d8e8e3b7cfb8c2ee83f56743d73596084aa
Jairus Musa <otangajairus@icloud.com>
2025-10-27 15:06:55 +0300

    2nd commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,12 +26,12 @@ const Home = () => {
       description: "Leftover meat products available daily after 5 PM.",
     },
     {
-      image: "https://images.unsplash.com/photo-1498579397066-22750a3cb424?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlc2glMjBmcnVpdHN8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000",
+      image: "https://images.unsplash.com/photo-1603048719539-9ecb4aa395e3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1768",
       title: "Potatoes from Nyeri",
       description: "Surplus potatoes available for pickup today.",
     },
     {
-      image: "https://images.unsplash.com/photo-1608198093002-ad4e005484ec?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YnJlYWR8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000",
+      image: "https://media.istockphoto.com/id/1440695422/photo/cornmeal-mush-and-vegetable-dish-served-on-a-white-plate-on-a-wooden-table-african.jpg?s=612x612&w=0&k=20&c=blC3gKnv08gZdUtHQSjJdr4PBuseySA_cyjC0DRHtEA=",
       title: "Flour Donations",
       description: "Ugali portions available daily after 6 PM.",
     },
